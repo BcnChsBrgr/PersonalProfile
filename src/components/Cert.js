53bf787120f0a4a1011f194852acf292d5d41a03
BcnChsBrgr <yuhinng41@gmail.com>
2021-10-25 00:46:35 +0800

    change name

--- src/components/Cert.js
+++ src/components/Cert.js
@@ -2,14 +2,17 @@ import Typography from '@mui/material/Typography';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
-    
+    HomeTitle: {
+        width: 'auto',
+        padding: '30px'
+    },
 });
 
 function Cert() {
     const classes = useStyles();
     return (
         <div>
-        <Typography className={`${classes.HomeTitle}`} variant="h3">Objective</Typography>
+            <Typography className={`${classes.HomeTitle}`} variant="h3">Certificate</Typography>
         </div>
     );
 }
